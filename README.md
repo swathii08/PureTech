baf5740837fc07784d3950981a4103b5f3756a71
swathii08 <138134977+swathii08@users.noreply.github.com>
2024-05-25 10:14:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# ingredients_analyzer
+
+
+PureTech Product Analyzer is a Streamlit web application that allows users to analyze product details, safety scores, harmful ingredients, category recommendations, and customer reviews for various products from different e-commerce websites.
+
+## Features
+
+- Fetch product details from Amazon, Flipkart, BigBasket, and Zepto by entering the product URL.
+- Display product name, price, and image (if available).
+- Analyze product ingredients using Google Gemini-Pro AI model.
+- Compute safety scores based on the presence of harmful ingredients.
+- Provide recommendations for alternative products in the same category.
+- Summarize the top 5 customer reviews for the product.
+
+## Usage
+
+1. Visit the Link web app.
+2. Enter the URL of the product you want to analyze.
+3. Click on the "Your Health in a Click" button to analyze the product.
+4. Explore the product details, safety score, harmful ingredients, recommendations, and customer reviews.
+
+## Installation
+
+To run the PureTech Product Analyzer locally, follow these steps:
+
+1. Clone the repository:
+
+2. Navigate to the project directory:
+
+3. Install the required dependencies:
+pip install -r requirements.txt
+
+
+4. Run the Streamlit app:
+streamlit run app.py
+
+
+5. Access the app in your web browser at `http://localhost:8501`.
+
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+
+
+
---
+++ test.py
@@ -0,0 +1,102 @@
+
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import random
+import time
+import google.generativeai as gen_ai
+
+# Importing custom scraping functions
+from amazon import extract_amazon_product_info
+from flipkart import extract_flipkart_product_info
+from bigbasket import extract_bigbasket_product_info
+from zepto import extract_product_info
+
+# Set Streamlit theme to centre
+st.set_page_config(page_title="PureTech")
+
+st.markdown("<h1 style='text-align: center;'>PureTech</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Uncover the secrets of beloved Products</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Securely sorted and wisely chosen</p>", unsafe_allow_html=True)
+
+
+url = st.text_input('Enter Product URL:')
+
+if url:
+    # Determine the website based on the URL entered
+    if 'amazon' in url:
+        website_name = 'Amazon'
+    elif 'flipkart' in url:
+        website_name = 'Flipkart'
+    elif 'bigbasket' in url:
+        website_name = 'BigBasket'
+    elif 'zepto' in url:
+        website_name = 'Zepto'
+    else:
+        website_name = None
+
+    if website_name:
+        try:
+            # Clear previous content
+            st.empty()
+
+            if website_name.lower() == 'amazon':
+                title, price, image_url, ingredients_list = extract_amazon_product_info(url)
+            elif website_name.lower() == 'flipkart':
+                title, price, image_url, ingredients_list = extract_flipkart_product_info(url)
+            elif website_name.lower() == 'bigbasket':
+                title, price, _, ingredients_list = extract_bigbasket_product_info(url)  # Ignore image_url for BigBasket
+                image_url = None  # Set image_url to None for BigBasket
+            elif website_name.lower() == 'zepto':
+                title, price, image_url, ingredients_list = extract_product_info(url)
+            else:
+                st.error("Invalid website name. Please enter a valid URL.")
+
+            if title and price:
+                st.subheader(title)
+                st.write(f"Price: {price}")
+                if image_url and website_name.lower() != 'bigbasket':  # Exclude image display for BigBasket
+                    st.image(image_url, caption='Product Image')
+           
+
+                # Insert your Gemini API key here
+                GOOGLE_API_KEY = "AIzaSyBOtkTpO9dSa5hnwikzTlkATTu4imJlPCg"
+
+                # Set up Google Gemini-Pro AI model
+                gen_ai.configure(api_key=GOOGLE_API_KEY)
+                model = gen_ai.GenerativeModel('gemini-pro')
+
+                # Initialize chat session in Streamlit if not already present
+                if "chat_session" not in st.session_state:
+                    st.session_state.chat_session = model.start_chat(history=[])
+
+                if st.button('Analyse Your Product'):
+                    with st.expander("Ingredients"):
+                        gemini_response = st.session_state.chat_session.send_message(f"Please provide each and every ingredient list of the {title}, if it is available on {url}. Fetch from it otherwise fetch from other sources.")
+                        ingredients_gemini = gemini_response.parts[0].text.strip().split("\n")
+                        merged_ingredients = list(set(ingredients_list + ingredients_gemini))
+                        gemini_final_response = st.session_state.chat_session.send_message(f" {merged_ingredients}. Please analyze and provide the ingredient list and put a ✅ emoji next to safe ingredients and a ❌ next to harmful ingredients which is not better for human health and Body. Don't include allergen information or extra details, stick to the ingredients only. and avoid same entries of ingridents")
+                        safety_score_response = st.session_state.chat_session.send_message(f"Compute the number of unhealthy ingredients which is not good for human health in take {gemini_final_response} for analysis and compute the safety_score = 100 - 4 * number of unhealthy ingredients")
+                        st.markdown(gemini_final_response.text)
+
+                    st.markdown(f"Safety Score: {safety_score_response.text}")
+
+                    harmful_analysis = st.session_state.chat_session.send_message(f"In a table format give the {title} unhealthy ingredients and their effects in another column keep the effects very short and precise")
+                    st.markdown(harmful_analysis.text)
+
+                    category_recommendation = st.session_state.chat_session.send_message(f"Please suggest a 2 product in the same category as that of {title} that is better than the current product along with an {url} link keep the content precise and short to the point do not brief.")
+
+                    reviews_summary = st.session_state.chat_session.send_message(f"Please analyze the top 4-5 customer reviews of {title} with given url {url} and do sentimental analysis. provide an overall summary keep it super short not exceeding more than four lines.")
+                    
+                    st.write(reviews_summary.text)
+                    st.write(category_recommendation.text)
+            else:
+                st.error("Unable to extract product information. Please check the URL and try again.")
+
+        except Exception as e:
+            st.error(f"An error occurred:  {e}")
+
+    else:
+        st.error("Invalid URL. Please enter a valid product URL.")
+
+
